b856937ddda994d2fca8e84d2feb71dbcceb3430
siddharthabhakta <37889108+siddharthabhakta@users.noreply.github.com>
2024-05-18 21:02:55 -0700

    Create netconf_client.c

---
+++ netconf_client.c
@@ -0,0 +1,110 @@
+
+#ifdef DOCUMENTATION
+Creating a NETCONF client that interacts with the NETCONF server using libnetconf2 involves several steps.
+Here's a complete example in a single C++ file to build a NETCONF client that connects to the server, sends an RPC, and processes the reply.
+
+Prerequisites:
+Ensure you have installed libnetconf2 and its dependencies.
+
+Explanation
+Include Headers: Includes necessary headers for libnetconf2 and libyang.
+Connect to Server: The connect_to_server function connects to the NETCONF server using SSH.
+Send RPC: The send_rpc function creates and sends an RPC to the server and processes the reply.
+Main Function: The main function connects to the server and sends an RPC, then cleans up the session.
+
+Build and Run
+Install Dependencies: Ensure you have installed libnetconf2 and libyang.
+  
+Compile: Use gcc or another C compiler to compile the program. Make sure to link against libnetconf2 and libyang.
+gcc -o netconf_client netconf_client.c -lxml2 -lssh -lnetconf2 -lyang
+Run: Run the client executable.
+./netconf_client
+
+#endif
+
+//Include necessary headers:
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libnetconf2/netconf.h>
+#include <libyang/libyang.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+//Define functions to create and send an RPC:
+struct nc_session *connect_to_server(const char *host, const char *username) {
+    struct nc_session *session = NULL;
+    nc_client_init();
+    nc_client_ssh_set_auth_password_clb([](const char *username, const char *hostname, void *priv) -> char * {
+        // Return password for the given username and hostname
+        // For simplicity, we're just using a fixed password here
+        return strdup("password");
+    }, NULL);
+
+    // Connect to the NETCONF server
+    session = nc_connect_ssh(host, username, NULL);
+    if (!session) {
+        fprintf(stderr, "Failed to connect to NETCONF server\n");
+    }
+    return session;
+}
+
+void send_rpc(struct nc_session *session) {
+    struct nc_rpc *rpc;
+    struct nc_reply *reply;
+    struct ly_ctx *ctx = nc_session_get_ctx(session);
+
+    // Create an RPC (get-config in this example)
+    rpc = nc_rpc_getconfig(NC_DATASTORE_RUNNING, NULL, NC_WD_ALL, NC_PARAMTYPE_CONST);
+    if (!rpc) {
+        fprintf(stderr, "Failed to create RPC\n");
+        return;
+    }
+
+    // Send the RPC
+    int ret = nc_send_rpc(session, rpc, 1000, &reply);
+    if (ret == -1) {
+        fprintf(stderr, "Failed to send RPC\n");
+    } else {
+        // Process the reply
+        switch (reply->type) {
+            case NC_RPL_OK:
+                printf("RPC OK\n");
+                break;
+            case NC_RPL_DATA:
+                printf("RPC Data: %s\n", lyd_print_mem(NULL, ((struct nc_reply_data *)reply)->data, LYD_XML, LYP_WITHSIBLINGS));
+                break;
+            default:
+                printf("RPC Error\n");
+                break;
+        }
+    }
+
+    // Cleanup
+    nc_rpc_free(rpc);
+    nc_reply_free(reply);
+}
+
+//Main function to connect and send RPC:
+int main(int argc, char **argv) {
+    const char *host = "127.0.0.1";
+    const char *username = "user";
+
+    struct nc_session *session = connect_to_server(host, username);
+    if (!session) {
+        return EXIT_FAILURE;
+    }
+
+    send_rpc(session);
+
+    // Cleanup
+    nc_session_free(session, NULL);
+    nc_client_destroy();
+
+    return EXIT_SUCCESS;
+}
+
